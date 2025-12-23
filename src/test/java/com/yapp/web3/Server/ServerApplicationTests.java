731cf48b5019f54bd08512a73a2f36821db83ba1
김준환 <musoyou1085@gmail.com>
2025-12-23 19:50:58 +0900

    first commit

---
+++ src/test/java/com/yapp/web3/Server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yapp.web3.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
